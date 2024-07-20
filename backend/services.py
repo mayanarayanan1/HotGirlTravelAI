2c2b3f09f72cf00ccce123cee6e653d203720b12
mayanarayanan1 <mayanarayanan04@gmail.com>
2024-07-20 15:12:16 -0700

    Update services.py

--- backend/services.py
+++ backend/services.py
@@ -1,1 +1,13 @@
 from fastapi, import fastapi
+
+def get_flights(flightPref):
+    return[]
+
+def filter_flights(flightsList, flightPref):
+    return 
+
+def get_hotels(hotelPrefs):
+    return[]
+
+def filter_hotels(hotelPrefs):
+    return
